ea494d0d572e40b33c53a42d4fe127f48a64b80f
Rakesh Rajnarayan Tiwari <149069042+rakeshr94@users.noreply.github.com>
2023-12-06 22:54:14 +0530

    Add files via upload

---
+++ word_count.py
@@ -0,0 +1,21 @@
+def count_words_in_file(file_path):
+    try:
+        with open(file_path, 'r')as file:
+            content = file.read()
+            word_count = len(content.split())
+            return word_count
+    except FileNotFoundError:
+        print("file not found. Please provide a valid path.")
+        return -1
+    
+if __name__=="__main__":
+    try:
+        file_path = input("Enter the path of the text file: ")
+        num_words = count_words_in_file(file_path)
+
+        if num_words >= 0:
+            print(f"Number of words in the file: {num_words}")
+    except KeyboardInterrupt:
+        print("\nProgram interuptted.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
